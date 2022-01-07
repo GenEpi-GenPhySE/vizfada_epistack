e9f5a1aa375a6e9b64d1bca5f8cbfdc2a34e20d7
Guillaume Devailly <gdevailly@hotmail.com>
2022-01-07 18:13:43 +0100

    cli tss

--- vizfada_epistack_tss_expression_test.R
+++ vizfada_epistack_tss_expression_test.R
@@ -1,3 +1,4 @@
+# interactive tests ---------------
 opt <- list()
 opt$anchors <- "/work/lmorel/data/pig/chipseq/epistack/median_tss.tsv"
 opt$bound <- "/work/lmorel/data/pig/chipseq/ERX3212574/bigwig/ERX3212576_R1.bigWig"
@@ -30,3 +31,15 @@ opt$png <- "~/work/epistack_test.png"
 # 4                    3                    2
 # IG_C_gene
 # 1
+
+# Rscript tests ---------------
+
+Rscript --vanilla ~/work/projects/vizfada/epistack.R \
+    -a /work/lmorel/data/pig/chipseq/epistack/median_tss.tsv \
+    -s /work/lmorel/data/pig/rnaseq/salmon/ERX3212540/quant.genes.sf \
+    -b /work/lmorel/data/pig/chipseq/ERX3212574/bigwig/ERX3212576_R1.bigWig \
+    -i /work/lmorel/data/pig/chipseq/ERX3212574/bigwig/ERX3212574_R1.bigWig \
+    -p ~/work/epistack_test_cli.png \
+    -t 'H3K27me3 I guess' \
+    -r start \
+    -y 0.5 -z 0.5 -c 2 -v -g 5 -m 99999 -f ci95
